730a9b6ba11703f0f371feae51abe4f8a9a0eef0
이난영 <hinagogo@naver.com>
2023-10-23 15:21:27 +0900

    Fist Commit

---
+++ src/main/java/com/thingspire/thingspire/ThingspireApplication.java
@@ -0,0 +1,13 @@
+package com.thingspire.thingspire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThingspireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThingspireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thingspire/thingspire/ThingspireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thingspire.thingspire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThingspireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
